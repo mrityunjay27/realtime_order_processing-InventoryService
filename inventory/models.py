5c632f4d2b05ce681945cd7d4b1d7466a5b4d6c8
mrityunjay <mrityunjay27062000@gmail.com>
2026-07-05 23:46:37 +0530

    feat: Created inventory service having inventory model. Postgres on docker

---
+++ inventory/models.py
@@ -0,0 +1,14 @@
+import uuid
+from django.db import models
+
+
+class Product(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
